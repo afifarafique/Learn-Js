d6346fb60d204ca9c188c8690955b1c78b01760f
afifarafique <139631388+afifarafique@users.noreply.github.com>
2024-07-30 06:12:10 +0000

    conversion of datatypes

---
+++ 01-basic/03-conversionoperation.js
@@ -0,0 +1,24 @@
+let score = 45
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//if we have value "33"=> it is converted in 33
+//if we have value "33abc"=> then it is converted in NaN
+//if we have value true then it is converted in 1 Or if we have False then it is converted in 0
+
+
+//let isLoggedIn  = 1
+//let isLoggedIn  = ""
+let isLoggedIn  = "Afifa"
+let booleanIsLoggedIn = Boolean( isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
