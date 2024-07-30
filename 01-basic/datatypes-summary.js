9c645a5dbb50df8cc48643c66e7ea8ed597efeba
afifarafique <139631388+afifarafique@users.noreply.github.com>
2024-07-30 10:32:49 +0000

    datatypes summary

---
+++ 01-basic/datatypes-summary.js
@@ -0,0 +1,47 @@
+//how to save data in memory and  how to access it
+//There are two catagoeries Primitivie & Non-Primitive
+//Primitive
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol,Bigint(for scientific values)
+
+
+
+const score =  4445
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+//declaring symbol
+const id = symbol('789')
+const anotherid = symbol('789')
+console.log(id === anotherid)
+
+//Reference(Non-Primitive)
+//Arrays, Objects, Functions
+
+
+const bigNumber = 44568992654459n
+
+const famous = ["Ayesha", "Alizba", "Ali"];
+let myObj ={
+name : "Afifa",                         //in curly braces there are objects and there datatype is any
+age : 17,
+}
+
+//storing function in a variable
+
+const myFunction = function() {
+    console.log("Hello World")
+}
+ 
+
+
+
+
+
+
+
+
+
+
