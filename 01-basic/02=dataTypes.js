e55811d22a9b64a9aab8dbb167d92994f4d41315
afifarafique <139631388+afifarafique@users.noreply.github.com>
2024-07-30 05:46:25 +0000

    config done

---
+++ 01-basic/02=dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";           //Treat all JS codeas as a newer version//
+
+//alert(3 + 3)          we are using nodejs, not Browser//
+
+console.log(3 + 3);  
+  console.log("Afifa")
+
+
+let name = "Afifa";
+let age = 17
+
+
+              //DataTypes//
+//Number => 2 to power 53        
+//bigint
+//string => ""     
+//boolean => True/False
+//null  => stanalone value
+//undefined =>
+//Symbol  => unique
+
+
+//Object
+console.log(typeof "Afifa");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+
+
