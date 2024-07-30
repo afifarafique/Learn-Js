dd6dee465edd5d11319cf61313272cff6f83259d
afifarafique <139631388+afifarafique@users.noreply.github.com>
2024-07-30 13:26:45 +0000

    date time

---
+++ 01-basic/07-date-time.js
@@ -0,0 +1,31 @@
+//Date
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+
+let Date1 = new Date(2024, 0 ,30)
+console.log(Date1.toDateString());
+
+let Date2 = new Date("06-30-2024")
+console.log(Date2.toLocaleString());
+
+
+let TimeStamp = Date.now()
+console.log(TimeStamp);
+console.log(Date2.getTime());
+
+
+let Date3 = new Date()
+console.log(Date3)
+console.log(Date3.getMonth() + 1);
+console.log(Date3.getDay());
+
+
+
+
+
+
+
+
