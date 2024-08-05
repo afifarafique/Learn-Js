c912c0a1462648c2525bf4a537c07d9767f4bce8
afifarafique <139631388+afifarafique@users.noreply.github.com>
2024-08-05 05:24:53 +0000

    while and do

---
+++ 03-iteration/02-whileanddowhileloop.js
@@ -0,0 +1,24 @@
+//while
+
+let i = 0
+while (i <= 10) {
+    console.log(`Value of i is ${i}`);
+    i = i + 2
+}
+
+
+let array = ["Afifa","Mama","Dad"]
+
+let arr = 0
+while (arr < array.length) {
+    console.log(`Value is ${array[arr]}`);
+    arr = arr + 1
+}
+
+
+//do while
+let num = 11;
+do {
+    console.log(`Num is ${num}`);
+    num++
+} while (num<=10);
